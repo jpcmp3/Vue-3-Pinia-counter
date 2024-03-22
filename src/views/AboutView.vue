2f4924cd5b6c09b1604716bb518c79da3224d2ce
Toshi <jpcmp3@gmail.com>
2024-03-22 06:50:15 +0200

    working 100%

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <button 
+    @click="storeCounter.increaseCount"
+    class="counter-button">
+      {{ storeCounter.count }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { useCounterStore } from '@/stores/counter'
+
+  const storeCounter = useCounterStore()
+</script>
+
+<style>
+.counter-button {
+  font-size: 50px;
+  margin: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <Counter/>
+    <div class="buttons">
+      <button @click="storeCounter.decreaseCount">-</button>
+      <button @click="storeCounter.increaseCount">+</button>
+    </div>
+
+    <div>
+      This counter is: {{ storeCounter.oddOrEven }}
+    </div>
+<hr>
+<div>
+  <h1>Edit counter: </h1>
+  <input 
+  v-model="storeCounter.count"
+  type="number">
+
+</div>
+  </div>
+</template>
+
+<script setup>
+import Counter from '@/components/Counter.vue'
+import { useCounterStore } from '@/stores/counter'
+
+  const storeCounter = useCounterStore()
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+  }
+  .buttons {
+  background-color: rgb(164, 171, 67); 
+  padding: 10px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
